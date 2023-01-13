84d2e1f21dc10275ecd864e41e11527b8643640c
olivia <opierez@gmail.com>
2023-01-13 11:43:57 -0500

    commit tree 836643dae45186a93d700a0a6a8f9d008cb33791

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Show.create(name: "Better Call Saul", network: "Fox", day:"Wednesday", rating: 8, season: 4)
+Show.create(name: "Wednesday", network: "Netflix", day:"Monday", rating: 10, season: 1)
+Show.create(name: "Arcane", network: "Netflix", day:"Tuesday", rating: 8, season: 1)
